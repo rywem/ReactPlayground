5e01cd266745db3bcc189e029d180c3e34806575
Ryan <cybervoid41@gmail.com>
2021-01-10 08:30:43 -0800

    Hook added to Person

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+//class App extends Component {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Mike", age: 34 },
+      { name: "Jane", age: 30 }
+    ],
+    otherState: 'some other value'
+  });
+
+  const switchNameHandler = () => {
+    //this.state.persons[0].name = 'Joe';
+    setPersonsState({persons: [
+      { name: "Maximilian", age: 28 },
+      { name: "Mike", age: 34 },
+      { name: "Jane", age: 25 }
+    ]});
+  } 
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm React App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
-export default App;
+
+
+
+export default app;
+
+/*
+state = {
+  persons: [
+    { name: "Max", age: 28 },
+    { name: "Mike", age: 34 },
+    { name: "Jane", age: 30 }
+  ],
+  otherState: 'some other value'
+}*/
+
+
