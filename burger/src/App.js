aae33944db75be0a83c452ca78615da477a31c38
Ryan <cybervoid41@gmail.com>
2021-01-12 06:02:22 -0800

    Two way binding added to Person

--- burger/src/App.js
+++ burger/src/App.js
@@ -15,27 +15,51 @@ const app = props => {
 
   const [otherState, setOtherState] = useState({otherState: 'some other value'});
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     //this.state.persons[0].name = 'Joe';
     setPersonsState({persons: [
-      { name: "Maximilian", age: 28 },
+      { name: newName, age: 28 },
       { name: "Mike", age: 34 },
       { name: "Jane", age: 25 }
     ]});
   } 
 
+  
+  /*
+  const nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 34 },
+        { name: "Jane", age: 25 }
+      ]
+    })
+  }*/
+  const nameChangedHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 34 },
+        { name: "Jane", age: 25 }
+      ]
+    })
+  }
+
+
   return (
     <div className="App">
       <h1>Hi, I'm React App</h1>
       <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
+      <button onClick={() => switchNameHandler('Maximilian')}>Switch Name</button>
       <Person 
         name={personsState.persons[0].name} 
         age={personsState.persons[0].age} />
       <Person 
         name={personsState.persons[1].name} 
         age={personsState.persons[1].age} 
-        click={switchNameHandler}>My Hobbies: Racing</Person>
+        click={switchNameHandler.bind(this, 'Max!')}
+        changed={nameChangedHandler}
+        >My Hobbies: Racing</Person>
       <Person 
         name={personsState.persons[2].name} 
         age={personsState.persons[2].age} />
