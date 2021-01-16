c1744a81d8e5040f7e87c2088f27e9631db80192
Ryan <cybervoid41@gmail.com>
2021-01-16 12:11:20 -0800

    working with list person, delete handler added

--- burger/src/App.js
+++ burger/src/App.js
@@ -14,14 +14,10 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (naame) =>{
-    this.setState({
-      persons: [
-        { name: naame, age: 28 },
-        { name: "Mike", age: 34 },
-        { name: "Jane", age: 30 }
-      ]
-    })
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   nameChangedHandler = (event) => {
@@ -54,18 +50,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age} />
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age} 
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler}
-            >My Hobbies: Racing</Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age} />
+          { this.state.persons.map((person, index) => {
+            return (
+              <Person 
+                click={() => this.deletePersonHandler(index)}
+                name={person.name} 
+                age={person.age} />
+            )
+          })}          
         </div>       
       );
     }
