b296e5a0a77840c8a0cdf258d904b26afc1d0b22
Ryan <cybervoid41@gmail.com>
2021-01-17 09:47:29 -0800

    Update name nameChangedHandler added

--- burger/src/App.js
+++ burger/src/App.js
@@ -20,14 +20,23 @@ class App extends Component {
     this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 34 },
-        { name: "Jane", age: 25 }
-      ],      
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.find(p => {
+      return p.id === id;
+    });
+
+    const person = personIndex;
+    /*
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    */
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   }
 
 
@@ -57,6 +66,7 @@ class App extends Component {
                 name={person.name} 
                 age={person.age}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
                  />
             )
           })}          
