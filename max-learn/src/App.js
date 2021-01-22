b953ba3be9a7e4d3cf2fe69d4d1cd16e2e6ab2c7
Ryan <cybervoid41@gmail.com>
2021-01-22 06:09:12 -0800

    using css modules in react

--- max-learn/src/App.js
+++ max-learn/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
+import classes from './App.css';
 
 class App extends Component {
   state = {
@@ -43,20 +43,8 @@ class App extends Component {
   }
 
   render() {
-    /*
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-*/
     let persons = null;
+    let btnClass = [classes.Button]
 
     if (this.state.showPersons) {
       persons = (
@@ -75,24 +63,24 @@ class App extends Component {
         </div>       
       );
       
-      
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if (this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
     
     
     return (      
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button className="button"
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button className={btnClass}
             onClick={this.togglePersonsHandler}>Toggle Persons
           </button>      
             {persons}
