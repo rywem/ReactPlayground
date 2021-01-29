b21f0309588c12367c80c01181f0638f17caf39c
Ryan <cybervoid41@gmail.com>
2021-01-29 10:28:55 -0800

    Changes

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div>
-     
+      
     </div>
   );
 }
-
+*/
 export default App;
