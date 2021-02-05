6706e8fd3b3d543801f91de03f0b5dee2b2a0077
Ryan <cybervoid41@gmail.com>
2021-02-05 08:03:19 -0800

    BurgerIngredient added, Layout added.

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hi
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
