d607be8d1ec23b43f1e30599398016cfb14d888c
Ryan <cybervoid41@gmail.com>
2021-03-03 07:24:02 -0800

    Re-create client-app project

--- SocialNetwork/client-app/src/index.tsx
+++ SocialNetwork/client-app/src/index.tsx
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <App />
+ReactDOM.render(  
+    <App />,
   document.getElementById('root')
 );
 
