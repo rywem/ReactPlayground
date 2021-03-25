306616363490c7d337b513a1090e09cc19550af3
Ryan <cybervoid41@gmail.com>
2021-03-25 09:22:50 -0700

    Create Activity Added

--- SocialNetwork/API/Controllers/ActivitiesController.cs
+++ SocialNetwork/API/Controllers/ActivitiesController.cs
@@ -14,24 +14,24 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ActivitiesController : ControllerBase
+    public class ActivitiesController : BaseApiController
     {
-        private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command(){ Activity = activity }));
         }
     }
 }
--- SocialNetwork/API/Controllers/BaseApiController.cs
+++ SocialNetwork/API/Controllers/BaseApiController.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.Extensions.DependencyInjection;
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();        
     }
 }
