59898742ed40dcbd0fcf4590db42f0d2e1dcbcae
Ryan <cybervoid41@gmail.com>
2021-03-29 06:52:37 -0700

    Reorganize react project files

--- SocialNetwork/client-app/src/app/layout/App.tsx
+++ SocialNetwork/client-app/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 function App() {
@@ -9,7 +7,6 @@ function App() {
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities').then(response => {
-      console.log(response);  
       setActivities(response.data);
     })
   }, []) 
